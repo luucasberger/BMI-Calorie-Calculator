4606473c400bcca3ec4a05e6f7c0306cc0ccf7f6
Lucas Berger <lucasberger2399@gmail.com>
2021-01-30 20:39:46 -0300

    BMI Calculator Auto Layout Fix

---
+++ BMI-Calorie-Calculator/Screens/CaloriesCalculator.swift
@@ -0,0 +1,30 @@
+//
+//  CaloriesCalculator.swift
+//  BMI-Calorie-Calculator
+//
+//  Created by Lucas Berger on 29/1/21.
+//
+
+import UIKit
+
+class CaloriesCalculator: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Calories Calculator"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BMI-Calorie-Calculator/Screens/Inicio.swift
@@ -0,0 +1,45 @@
+//
+//  Inicio.swift
+//  BMI-Calorie-Calculator
+//
+//  Created by Lucas Berger on 26/1/21.
+//
+
+import UIKit
+
+class Inicio: UIViewController {
+    @IBOutlet weak var bmiButton: UIButton!
+    @IBOutlet weak var caloriesButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.1770251393, green: 0.2089989185, blue: 0.2513588071, alpha: 1)
+        navigationItem.title = "BMI-Calorie Calculator"
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "DINAlternate-Bold", size: 23.0)!, .foregroundColor: UIColorFromRGB(rgbValue: 0x00adb5)]
+    }
+    
+    /*
+     Function to set navigationBar title color as RGB.
+     Reason of creation: cannot put 'ColorLiteral'.
+     */
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "BmiToCalculator") {
+            let backItem = UIBarButtonItem()
+            backItem.title = "Something Else"
+            navigationItem.backBarButtonItem = backItem // This will show in the next view controller being pushed
+        }
+    } */
+    
+}
